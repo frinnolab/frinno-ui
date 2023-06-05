db25d5cfc2dae833404b31825f1049c318561460
Frank Leons <dev.frinno@gmail.com>
2023-06-05 12:51:34 +0300

    #Minor Fixes#

--- src/app/pages/Home/home.component.ts
+++ src/app/pages/Home/home.component.ts
@@ -37,12 +37,60 @@ export class HomeComponent implements OnInit {
     title:`...`
   };
 
+  //Builds
+  dummyBuilds:any[] =
+  [
+    {
+      id:1,
+      title:`Web`
+    },
+    {
+      id:2,
+      title:`Mobile`
+    },
+    {
+      id:3,
+      title:`Design`
+    },
+  ]
+
+  currentBuild:any= {
+    id:0,
+    title:`...`
+  };
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(){
+    this.getTopicInterval();
+    this.getBuildInterval();
+  }
+
+  getBuildInterval = ()=>{
+    setInterval(()=>{
+      switch (this.currentBuild.id) {
+        case 0:
+          this.currentBuild = this.dummyBuilds[0]
+          break;
+        case 1:
+            this.currentBuild = this.dummyBuilds[1]
+          break;
+        case 2:
+              this.currentBuild = this.dummyBuilds[2]
+          break;
+
+        default:
+          this.currentBuild = this.dummyBuilds[0]
+          break;
+      }
+    },3500);
+  }
+
+  getTopicInterval = ()=>{
     setInterval(()=>{
       switch (this.currentTopic.id) {
         case 0:
