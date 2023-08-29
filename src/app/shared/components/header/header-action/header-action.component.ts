0a90504835889ab3d116ff0ac92b7e8e231c57fe
Frank Leons <dev.frinno@gmail.com>
2023-08-30 01:30:10 +0300

    Add: Added Minor changes to F.E###

--- src/app/shared/components/header/header-action/header-action.component.ts
+++ src/app/shared/components/header/header-action/header-action.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderActionComponent implements OnInit {
 
+  
+  socials:any[] = [
+    {
+      id:0,
+      name:`LinkedIn`,
+      icon:`assets/svgs/brands/linkedin.svg`,
+      url:``
+    },
+    {
+      id:1,
+      name:`Twitter - X`,
+      icon:`assets/svgs/brands/twitter.svg`,
+      url:``
+    },
+    {
+      id:2,
+      name:`Youtube`,
+      icon:`assets/svgs/brands/youtube.svg`,
+      url:``
+    },
+    {
+      id:3,
+      name:`Twitch`,
+      icon:`assets/svgs/brands/twitch.svg`,
+      url:``
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
